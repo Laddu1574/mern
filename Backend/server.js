9f9edd75dfd476cd67746475f432ae1750770a6d
Jagadeesh <jagadeesh.thaniparthi@gmail.com>
2024-12-26 16:00:51 +0530

    Initial commit

---
+++ Backend/server.js
@@ -0,0 +1,23 @@
+// const express = require('express');//
+import express from "express";
+import dotenv from "dotenv";
+import { connectDB } from "./config/db.js";
+import productRoutes from "./routes/product.route.js";
+import path from "path";
+
+dotenv.config();
+const app = express();
+const PORT = process.env.PORT || 5000;
+const __dirname = path.resolve();
+app.use(express.json()); //allows us to create JSON data in the req.body
+app.use("/api/products", productRoutes);
+if (process.env.NODE_ENV === "production") {
+  app.use(express.static(path.join(__dirname, "/Frontend/dist")));
+  app.get("*", (req, res) => {
+    res.sendFile(path.resolve(__dirname, "Frontend", "dist", "index.html"));
+  });
+}
+app.listen(PORT, () => {
+  connectDB();
+  console.log("Server started at http://localhost:" + PORT);
+});
